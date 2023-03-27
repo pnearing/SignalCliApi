d4f965ef71dfc55624fe28974ded6bc32819dc6a
pnearing <me@peternearing.ca>
2023-03-27 18:54:45 -0400

    Refactor and clean up style for signalTest.py.py

--- signalTest.py
+++ signalTest.py
@@ -15,66 +15,67 @@ from signalSentMessage import SentMessage
 from signalSyncMessage import SyncMessage
 from signalTypingMessage import TypingMessage
 
-def allMsgCb(account: Account, message:ReceivedMessage|Receipt|TypingMessage|SyncMessage):
-    # print("ALL")
+
+def all_msg_cb(account: Account, message: ReceivedMessage | Receipt | TypingMessage | SyncMessage):
+    print("ALL")
     # print(message)
     return
 
-def receivedMsgCb(account:Account, message:ReceivedMessage):
+
+def received_msg_cb(account: Account, message: ReceivedMessage):
     print("RECEIVED")
     # message.mark_read()
     print(message)
     print("Received message FROM: %s AT: %s on DEVICE: %s" % (
-                                                            message.sender.get_display_name(),
-                                                            message.timestamp.get_display_time(),
-                                                            message.device.get_display_name(),
-                                                    ))
+        message.sender.get_display_name(),
+        message.timestamp.get_display_time(),
+        message.device.get_display_name(),
+    ))
     if message.recipient_type == 'group':
         print("In GROUP: %s" % message.recipient.get_display_name())
-    if (message.quote != None):
-        if (message.quote.attachments != None):
+    if message.quote is not None:
+        if message.quote.attachments is not None:
             for attachment in message.quote.attachments:
                 displayName = "<UNKNOWN-ATTACHMENT>"
-                if (attachment.file_name != None and attachment.file_name != ''):
+                if attachment.file_name is not None and attachment.file_name != '':
                     displayName = attachment.file_name
-                elif (attachment.local_path != None and attachment.exists == True):
+                elif attachment.local_path is not None and attachment.exists:
                     displayName = attachment.local_path
-                elif (attachment.thumbnail != None and attachment.thumbnail.exists == True):
+                elif attachment.thumbnail is not None and attachment.thumbnail.exists:
                     displayName = "Thumbnail: %s" % attachment.thumbnail.local_path
                 print("Trying to display attachment: %s...\n    Success=" % displayName, end='', flush=True)
                 # returnValue = attachment.display()
                 # print(returnValue)
 
-        quotedMessage = account.messages.get_quoted(message.quote)
-        if (quotedMessage == None):
+        quoted_message = account.messages.get_quoted(message.quote)
+        if quoted_message is None:
             print("Quoting a message not found in history.")
         else:
-            if (isinstance(quotedMessage, ReceivedMessage) == True):
-                print("Quoting received message FROM:%s AT: %s" % (quotedMessage.sender.get_display_name(),
-                                                                    quotedMessage.timestamp.get_display_time()))
-                if (quotedMessage.recipient_type == 'group'):
-                    print("in GROUP: %s" % quotedMessage.recipient.get_display_name())
-            elif (isinstance(quotedMessage, SentMessage) == True):
-                print("Quoting message sent AT: %s" % quotedMessage.timestamp.get_display_time())
-                if (quotedMessage.recipient_type == 'group'):
-                    print("In GROUP: %s" % quotedMessage.recipient.get_display_name())
-        quotedText = message.quote.parse_mentions()
-        print("Quoted TEXT: %s" % quotedText)
+            if isinstance(quoted_message, ReceivedMessage):
+                print("Quoting received message FROM:%s AT: %s" % (quoted_message.sender.get_display_name(),
+                                                                   quoted_message.timestamp.get_display_time()))
+                if quoted_message.recipient_type == 'group':
+                    print("in GROUP: %s" % quoted_message.recipient.get_display_name())
+            elif isinstance(quoted_message, SentMessage):
+                print("Quoting message sent AT: %s" % quoted_message.timestamp.get_display_time())
+                if quoted_message.recipient_type == 'group':
+                    print("In GROUP: %s" % quoted_message.recipient.get_display_name())
+        quoted_text = message.quote.parse_mentions()
+        print("Quoted TEXT: %s" % quoted_text)
 
     print("--------Begin Message--------")
-    displayText = message.parse_mentions()
-    print(displayText)
+    display_text = message.parse_mentions()
+    print(display_text)
     print("---------End Message---------")
 
-
-    if (message.attachments != None):
+    if message.attachments is not None:
         for attachment in message.attachments:
             displayName = "<UNKNOWN-ATTACHMENT>"
-            if (attachment.file_name != None and attachment.file_name != ''):
+            if attachment.file_name is not None and attachment.file_name != '':
                 displayName = attachment.file_name
-            elif (attachment.local_path != None and attachment.exists == True):
+            elif attachment.local_path is not None and attachment.exists:
                 displayName = attachment.local_path
-            elif (attachment.thumbnail != None and attachment.thumbnail.exists == True):
+            elif attachment.thumbnail is not None and attachment.thumbnail.exists:
                 displayName = "Thumbnail: %s" % attachment.thumbnail.local_path
             print("Trying to display attachment: %s...\n    Success=" % displayName, end='', flush=True)
             # returnValue = attachment.display()
@@ -89,44 +90,53 @@ def receivedMsgCb(account:Account, message:ReceivedMessage):
     #         print("A message not in history")
     return
 
-def receiptMsgCb(account, message: Receipt):
+
+def receipt_msg_cb(account, message: Receipt):
     print("RECEIPT")
     print(message.body)
     return
 
-def syncMsgCb(account, message):
+
+def sync_msg_cb(account, message):
     print("SYNC")
     print(message)
     return
 
-def typeMsgCb(account, message: TypingMessage):
+
+def type_msg_cb(account, message: TypingMessage):
     print("TYPING")
     print(message.body)
     return
 
-def storyMsgCb(account, message):
+
+def story_msg_cb(account, message):
     print("STORY")
     print(message)
     return
 
-def paymentMsgCb(account, message):
+
+def payment_msg_cb(account, message):
     print("PAYMENT")
     print(message)
     return
 
-def reactionMsgCb(account, message:Reaction):
+
+def reaction_msg_cb(account, message: Reaction):
     print("REACTION")
     print(message.body)
     return
 
-def callMsgCb(account, message):
+
+def call_msg_cb(account, message):
     print("CALL")
     print(message)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Signal test code.")
     parser.add_argument("--register", help="--register NUMBER, number to register.")
-    parser.add_argument("--captcha", help="--captcha CAPTCHA, obtained from: https://signalcaptchas.org/registration/generate.html")
+    parser.add_argument("--captcha",
+                        help="--captcha CAPTCHA, obtained from: https://signalcaptchas.org/registration/generate.html")
     parser.add_argument("--verify", help="--verify NUMBER, number to verify")
     parser.add_argument("--code", help="--code CODE, 6 digit verification code.")
     parser.add_argument("--link", help="--link, Link an existing account.", action='store_true')
@@ -137,49 +147,49 @@ if __name__ == '__main__':
     parser.add_argument("--doSendGroup", help="--doSendGroup, Send a test message to c&c group", action='store_true')
     args = parser.parse_args()
 
-    signalConfigPath = '/home/streak/.local/share/signal-cli'
+    signal_config_path = '/home/streak/.local/share/signal-cli'
     # signal_config_path = "/home/streak/signal-cli"
     # signal_config_path = None
-    logFilePath = '/home/streak/signal-cli/output.log'
-    previewUrl = 'https://thepostmillennial.com/balenciaga-tries-to-walk-back-ads-promoting-child-exploitation-by-suing-ad-creator'
+    log_file_path = '/home/streak/signal-cli/output.log'
+    preview_url = 'https://thepostmillennial.com/balenciaga-tries-to-walk-back-ads-promoting-child-exploitation-by-suing-ad-creator'
 
     print("loading accounts")
 
-    signal = SignalCli(signalConfigPath)
+    signal = SignalCli(signal_config_path)
 
     for account in signal.accounts:
-        print (account.number)
+        print(account.number)
 
-    if (args.register != None):
+    if args.register is not None:
         response = signal.register_account(args.register, args.captcha)
-        if (response[0] == True):
-            print ("Registration successful. Please verify.")
+        if response[0]:
+            print("Registration successful. Please verify.")
         else:
             print("Registration unsuccessful: %s" % response[1])
         exit(0)
 
-    elif (args.verify != None):
-    # Get unregistered accounts:
+    elif args.verify is not None:
+        # Get unregistered accounts:
         account = signal.accounts.get_by_number(args.verify)
-        if (account == None):
+        if account is None:
             print("Unknown account: %s" % args.verify)
             exit(1)
-        if (account.registered == True):
+        if account.registered:
             print("Account already registered.")
             exit(1)
         response = account.verify(args.code)
         print(response[0], response[1])
         exit(0)
-    elif( args.link == True):
+    elif args.link:
         response = signal.start_link_account("SignalApi")
         print("LINK: ", response[0])
         print("QRCODE: \n", response[1])
         print("pngCode: ", response[2])
 
         response = signal.finsh_link()
-        print (response)
+        print(response)
         exit(0)
-    elif (args.addContact != None):
+    elif args.addContact is not None:
         account = signal.accounts.get_by_number(args.account)
         print("Existing contacts:")
         for contact in account.contacts:
@@ -188,16 +198,16 @@ if __name__ == '__main__':
         print("Added:", added)
         print(contact)
         exit(0)
-    
+
     account = signal.accounts.get_by_number(args.account)
     print("GROUPS:")
     for group in account.groups:
-        print ("Id: ", group.id, "Name: ", group.name)
+        print("Id: ", group.id, "Name: ", group.name)
     print("CONTACTS:")
     for contact in account.contacts:
         print(contact.get_display_name())
 
-    if (args.account == '+16134548055'):
+    if args.account == '+16134548055':
         print("Updating profile given name...")
         updated = account.profile.set_given_name("Peter's")
         print("Updated=", updated)
@@ -211,30 +221,27 @@ if __name__ == '__main__':
         update = account.profile.set_emoji('🤖')
         print("Updated=", updated)
 
-    signal.start_receive(account, allMsgCb, receivedMsgCb, receiptMsgCb, syncMsgCb, typeMsgCb, storyMsgCb, paymentMsgCb,
-                         reactionMsgCb)
+    signal.start_receive(account, all_msg_cb, received_msg_cb, receipt_msg_cb, sync_msg_cb, type_msg_cb, story_msg_cb, payment_msg_cb,
+                         reaction_msg_cb)
 
-    
-
-    if (args.doSend != None):
+    if args.doSend is not None:
         contact = account.contacts.get_by_id(args.doSend)
-        preview = Preview(config_path=signalConfigPath,
+        preview = Preview(config_path=signal_config_path,
                           generate_preview=True,
-                          url=previewUrl)
+                          url=preview_url)
         account.messages.send_message(
-                recipients=contact,
-                body="Test message.\n%s" % previewUrl,
-                preview=preview,
-            )
+            recipients=contact,
+            body="Test message.\n%s" % preview_url,
+            preview=preview,
+        )
         stickerPack = signal.sticker_packs.get_pack_by_name("Josh Saunders")
         sticker = stickerPack[0]
         account.messages.send_message(contact, sticker=sticker)
-    
 
     # stickerPack = signal.stickers.get_by_name("Josh Saunders")
     # sticker = stickerPack[0]
     # account.messages.send_message(recipients=[recipient], sticker=sticker)
-    if (args.doSendGroup == True):
+    if args.doSendGroup:
         marshJpg = '/home/streak/Pictures/marshmallow.jpg'
         groupId = "ECxpUY76Wwti8hxCfDmOgE9cZx2CCYHD1GxlWwwtFjs="
         group = account.groups.get_by_id(groupId)
@@ -245,10 +252,10 @@ if __name__ == '__main__':
 
         results = account.messages.send_message(group, messageText, mentions=mentions, attachments=marshJpg)
         for result in results:
-            print ("Success:", result[0], "\tContact:", result[1].get_display_name(), "\tmessage:", result[2])
+            print("Success:", result[0], "\tContact:", result[1].get_display_name(), "\tmessage:", result[2])
 
     try:
-        while (True):
+        while True:
             pass
     except KeyboardInterrupt:
         print("\nKeyboard Interrupt")
