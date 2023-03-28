439e79f3ec915bca8668ed7e729b4cc85f196f1c
pnearing <me@peternearing.ca>
2023-03-28 09:01:54 -0400

    remove debug

--- signalAttachment.py
+++ signalAttachment.py
@@ -81,9 +81,6 @@ class Attachment(object):
         return
 
     def __from_raw_attachment__(self, raw_attachment: dict) -> None:
-        print(raw_attachment)
-        print("DEBUG IN FROM_RAW_ATTACHMENT")
-        exit(252)
         self.content_type = raw_attachment['contentType']
         self.id = raw_attachment['id']
         self.filename = raw_attachment['filename']
