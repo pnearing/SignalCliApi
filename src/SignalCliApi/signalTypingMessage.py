5a8cee1448db5d763069bdedb5ae9fd0d7af1545
Peter Nearing <me@peternearing.ca>
2023-11-21 11:06:47 -0500

    Doc strings remove DEBUG clean up imports. remove unnecessary properties from __init__(). add logging

--- src/SignalCliApi/signalTypingMessage.py
+++ src/SignalCliApi/signalTypingMessage.py
@@ -1,21 +1,19 @@
 #!/usr/bin/env python3
-
+"""
+File: signalTypingMessage.py
+Store and handle a typing message.
+"""
+import logging
 from typing import Optional, Any
 import socket
 
-from .signalCommon import __type_error__, RecipientTypes
-from .signalContact import Contact
+from .signalCommon import RecipientTypes, MessageTypes
 from .signalContacts import Contacts
 from .signalDevice import Device
 from .signalDevices import Devices
-from .signalGroup import Group
 from .signalGroups import Groups
-# from .signalMention import Mention
 from .signalMessage import Message
-# from .signalReaction import Reaction
-# from .signalSticker import Sticker
 from .signalTimestamp import Timestamp
-DEBUG: bool = False
 
 
 class TypingMessage(Message):
@@ -28,46 +26,53 @@ class TypingMessage(Message):
                  groups: Groups,
                  devices: Devices,
                  this_device: Device,
-                 from_dict: Optional[dict] = None,
-                 raw_message: Optional[dict] = None,
-                 sender: Optional[Contact] = None,
-                 recipient: Optional[Contact | Group] = None,
-                 device: Optional[Device] = None,
-                 timestamp: Optional[Timestamp] = None,
-                 action: Optional[str] = None,
-                 time_changed: Optional[Timestamp] = None,
+                 from_dict: Optional[dict[str, Any]] = None,
+                 raw_message: Optional[dict[str, Any]] = None,
                  ) -> None:
-        # Arg Checks:
-        # Check action
-        if action is not None:
-            if not isinstance(action, str):
-                __type_error__("action", 'str', action)
-            action = action.upper()
-            if action != 'STARTED' and action != 'STOPPED':
-                raise ValueError("action must be either STARTED or STOPPED")
-        # Check time changed:
-        if time_changed is not None and not isinstance(time_changed, Timestamp):
-            __type_error__("time_changed", "Timestamp", time_changed)
+        """
+        Initialize a Typing Message.
+        :param command_socket: socket.socket: The socket to run commands on.
+        :param account_id: str: This accounts' ID.
+        :param config_path: str: The full path to the signal-cli config directory.
+        :param contacts: Contacts: This accounts' Contacts object.
+        :param groups: Groups: This accounts' Groups object.
+        :param devices: Devices: This accounts' Devices object.
+        :param this_device: Device: The Device object that represents the device we're on.
+        :param from_dict: Optional[dict[str, Any]]: The dict created to __to_dict__().
+        :param raw_message: Optional[dict[str, Any]]: A dict provided by Signal.
+        """
         # Set external properties:
-        self.action: str = action
-        self.time_changed: Timestamp = time_changed
-        self.body: str = ''
-        # Run super init:
+        self.action: str = "NOT SET"
+        """The action being preformed. Either STARTED or STOPPED."""
+        self.time_changed: Optional[Timestamp] = None
+        """The Timestamp of the action change."""
+
+        # Run super:
         super().__init__(command_socket, account_id, config_path, contacts, groups, devices, this_device, from_dict,
-                         raw_message, sender, recipient, device, timestamp, Message.TYPE_TYPING_MESSAGE)
+                         raw_message, None, None, None, None, MessageTypes.TYPING)
+
         # update body:
         self.__update_body__()
         return
 
-    def __from_raw_message__(self, raw_message: dict) -> None:
+    def __from_raw_message__(self, raw_message: dict[str, Any]) -> None:
+        """
+        Load properties from a dict provided by Signal.
+        :param raw_message: dict[str, Any]: The dict to load from.
+        :return: None
+        """
         super().__from_raw_message__(raw_message)
         typing_dict: dict[str, Any] = raw_message['typingMessage']
         self.action = typing_dict['action']
         self.time_changed = Timestamp(timestamp=typing_dict['timestamp'])
         return
 
-    def __to_dict__(self) -> dict:
-        typing_message = super().__to_dict__()
+    def __to_dict__(self) -> dict[str, Any]:
+        """
+        Create a JSON friendly dict for this typing message.
+        :return: dict[str, Any]: A dict to provide to __from_dict__().
+        """
+        typing_message: dict[str, Any] = super().__to_dict__()
         typing_message['action'] = self.action
         if self.time_changed is not None:
             typing_message['timeChanged'] = self.time_changed.__to_dict__()
@@ -75,16 +80,25 @@ class TypingMessage(Message):
             typing_message['timeChanged'] = None
         return typing_message
 
-    def __from_dict__(self, from_dict: dict) -> None:
+    def __from_dict__(self, from_dict: dict[str, Any]) -> None:
+        """
+        Load fom a JSON friendly dict.
+        :param from_dict: dict[str, Any]: A dict provided by __to_dict__().
+        :return: None
+        """
         super().__from_dict__(from_dict)
         self.action = from_dict['action']
+        self.time_changed = None
         if from_dict['timeChanged'] is not None:
             self.time_changed = Timestamp(from_dict=from_dict['timeChanged'])
-        else:
-            self.time_changed = None
         return
 
     def __update_body__(self) -> None:
+        """
+        Update the body based on the current action.
+        :return: None
+        """
+        logger: logging.Logger = logging.getLogger(__name__ + '.' + self.__update_body__.__name__)
         if self.sender is not None and self.action is not None and self.time_changed is not None:
             if self.recipient is not None and self.recipient_type is not None:
                 if self.recipient_type == RecipientTypes.CONTACT:
@@ -96,6 +110,9 @@ class TypingMessage(Message):
                         self.time_changed.get_display_time(), self.sender.get_display_name(),
                         self.action.lower(), self.recipient.get_display_name())
                 else:
-                    raise ValueError("invalid recipient_type: %s" % str(self.recipient_type))
+                    error_message: str = "invalid recipient_type: %s" % str(self.recipient_type)
+                    logger.critical("Raising ValueError(%s)." % error_message)
+                    raise ValueError(error_message)
         else:
             self.body = "Invalid typing message."
+        return
