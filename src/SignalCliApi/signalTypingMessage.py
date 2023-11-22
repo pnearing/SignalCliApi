c99ccdd0890aed7c69c24edffafe6bbecc1b21ce
Peter Nearing <me@peternearing.ca>
2023-11-21 19:11:32 -0500

    re-add parameters to __init__(), so we can create a full typing message when we need to in ReceiveThread. Also mark all typing messages as delivered, read, and viewed.

--- src/SignalCliApi/signalTypingMessage.py
+++ src/SignalCliApi/signalTypingMessage.py
@@ -7,10 +7,12 @@ import logging
 from typing import Optional, Any
 import socket
 
-from .signalCommon import RecipientTypes, MessageTypes, TypingStates
+from .signalCommon import RecipientTypes, MessageTypes, TypingStates, __type_error__
+from .signalContact import Contact
 from .signalContacts import Contacts
 from .signalDevice import Device
 from .signalDevices import Devices
+from .signalGroup import Group
 from .signalGroups import Groups
 from .signalMessage import Message
 from .signalTimestamp import Timestamp
@@ -28,6 +30,12 @@ class TypingMessage(Message):
                  this_device: Device,
                  from_dict: Optional[dict[str, Any]] = None,
                  raw_message: Optional[dict[str, Any]] = None,
+                 sender: Optional[Contact] = None,
+                 recipient: Optional[Contact | Group] = None,
+                 device: Optional[Device] = None,
+                 timestamp: Optional[Timestamp] = None,
+                 action: TypingStates = TypingStates.NOT_SET,
+                 time_changed: Optional[Timestamp] = None
                  ) -> None:
         """
         Initialize a Typing Message.
@@ -40,21 +48,45 @@ class TypingMessage(Message):
         :param this_device: Device: The Device object that represents the device we're on.
         :param from_dict: Optional[dict[str, Any]]: The dict created to __to_dict__().
         :param raw_message: Optional[dict[str, Any]]: A dict provided by Signal.
+        :param sender: Optional[Contact]: The sender of this message.
+        :param recipient: Optional[Contact | Group]: The recipient of the message.
+        :param device: Optional[Device]: The device that generated this message.
+        :param timestamp: Optional[Timestamp]: The timestamp of this message.
+        :param action: TypingStates: The typing state, either started or stopped.
+        :param time_changed: Optional[Timestamp]: The time the typing state changed.
         """
+        # Setup logging:
+        logger: logging.Logger = logging.getLogger(__name__ + '.' + self.__init__.__name__)
+
+        # Type check parameters:
+        if not isinstance(action, TypingStates):
+            logger.critical("Raising TypeError:")
+            __type_error__('action', 'TypingStates', action)
+
+        if time_changed is not None and not isinstance(time_changed, Timestamp):
+            logger.critical("Raising TypeError:")
+            __type_error__('time_changed', 'Optional[Timestamp]', time_changed)
+
         # Set external properties:
         # The typing action:
-        self.action: TypingStates = TypingStates.NOT_SET
+        self.action: TypingStates = action
         """The action being preformed. Either STARTED or STOPPED."""
         # The time the typing action changed.
-        self.time_changed: Optional[Timestamp] = None
+        self.time_changed: Optional[Timestamp] = time_changed
         """The Timestamp of the action change."""
 
         # Run super:
         super().__init__(command_socket, account_id, config_path, contacts, groups, devices, this_device, from_dict,
-                         raw_message, None, None, None, None, MessageTypes.TYPING)
+                         raw_message, sender, recipient, device, timestamp, MessageTypes.TYPING)
 
         # update body:
         self.__update_body__()
+
+        # Mark as delivered, read, and viewed.
+        if self.timestamp is not None:
+            self.mark_delivered(self.timestamp)
+            self.mark_read(self.timestamp)
+            self.mark_viewed(self.timestamp)
         return
 
     def __from_raw_message__(self, raw_message: dict[str, Any]) -> None:
