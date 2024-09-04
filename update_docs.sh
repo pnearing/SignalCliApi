6332decb31d14f807f44a157aa7bc0acb477f450
Peter Nearing <me@peternearing.ca>
2024-09-04 11:33:44 -0400

    Add API documentation for signal_cli_api
    Generated comprehensive HTML documentation for the `signal_cli_api` module using pdoc. Includes details on class methods, variables, and overall structure to enhance developer understanding and support.

--- update_docs.sh
+++ update_docs.sh
@@ -5,5 +5,5 @@ rm -rf docs/html
 echo "Creating documentation..."
 pdoc --footer-text 'Version 1.0' \
 --output-directory 'docs/html/' \
-src/signal_cli_api/*.py
+src/signal_cli_api/
 echo "Complete."
